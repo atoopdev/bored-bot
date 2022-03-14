a6a3f0220fdcd3bdecfa2994ee1851d1acc882fc
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-03-14 13:35:35 -0700

    HTML/CSS
    button, styling

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
 
-fetch('https://apis.scrimba.com/bored/api/activity')
-.then(response => response.json())
-.then(data =>{
-    console.log(data)
-    document.getElementById("activityEL").innerHTML = `
-    <h1>${data.activity}</h1>`
-})
+
+
+function getActivity(){
+    fetch('https://apis.scrimba.com/bored/api/activity')
+    .then(response => response.json())
+    .then(data =>{
+        console.log(data)
+        document.getElementById("activityEL").innerHTML = `
+        <h2>${data.activity}</h2>`
+    })
+}
